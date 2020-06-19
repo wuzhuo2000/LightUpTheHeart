177aa96e4763fdb4b6d08de996c9a8905b17de37
wuzhuo2000 <1421080741@qq.com>
2020-06-19 16:46:48 +0800

    v1

--- pages/day/day.js
+++ pages/day/day.js
@@ -1,97 +1,10 @@
 // pages/day/day.js
-import * as echarts from '../../components/ec-canvas/echarts';
-function initChart(canvas, width, height) {
-  const chart = echarts.init(canvas, null, {
-    width:width,
-    height: height
-  });
-  canvas.setChart(chart);
-
-  var option = {
-  //定义图标的标题和颜色
-    title: {
-      text: '今日访问量',
-      left: 'center'
-    },
-    color: ["#37A2DA"],
-    //定义你图标的线的类型种类
-    legend: {
-    data: ['A'],
-    top: 50,
-    left: 'center',
-    backgroundColor: 'red',
-    z: 100
-    },
-    grid: {
-    containLabel: true
-  },
-  //当你选中数据时的提示框
-    tooltip: {
-      show: true,
-      trigger: 'axis'
-    },
-    //	x轴
-    xAxis: {
-      type: 'category',
-      boundaryGap: false,
-      data: ['一', '二', '三', '四', '五', '六', '日', '一', '二', '三'],//x轴数据
-      // x轴的字体样式
-      axisLabel: {
-        show: true,
-        textStyle: {
-          color: '#000',
-          fontSize: '14',
-        }
-      },
-      // 控制网格线是否显示
-      splitLine: {
-        show: true,
-        //  改变轴线颜色
-        lineStyle: {
-          // 使用深浅的间隔色
-          color: ['#aaaaaa']
-        }
-      },
-      // x轴的颜色和宽度
-      axisLine: {
-        lineStyle: {
-          color: '#000',
-          width: 1,   //这里是坐标轴的宽度,可以去掉
-        }
-      }
-      // show: false //是否显示坐标
-    },
-    yAxis: {
-      x: 'center',
-      type: 'value',
-      //网格线
-      splitLine: {
-        lineStyle: {
-          type: 'dashed'
-        }
-      },
-      // show: false
-    },
-    series: [{
-      name: 'A',
-      type: 'line',
-      smooth: true,
-      data: [15, 2, 30, 16, 10, 17, 15, 22, 27, 9]
-    }]
-  };
-  chart.setOption(option);
-  return chart;
-}
-
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    ec: {
-      onInit: initChart
-    },
     circlesize:200,
     // tab 切换
     tabArr: {
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: '欢迎加入睡眠健康',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
